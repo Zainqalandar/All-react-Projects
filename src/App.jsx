39beca886bb5409b660c42cebf9931b7a77daf3a
Zainqalandar <zainqlandar@gmail.com>
2023-08-31 12:31:29 +0500

    ViteProjects

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+    <>
+     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World</h1>
+    </>
+  )
+}
+
+export default App

b75cffce40b9f3d65edf0014b94c42904c758115
Zainqalandar <zainqlandar@gmail.com>
2023-09-04 19:29:35 +0500

    Watts app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,62 @@
+import { useState } from 'react'
+import { getDatabase,ref,push,set } from "firebase/database";
 import './App.css'
 function App() {
+  const [state, setState] = useState({
+    Name: "",
+    Message: ""
+  })
+  const [chat, setChat] = useState([{ Name: "zain", Message: 'Hi! I am zain hare....' },
+  { Name: "suhan", Message: 'Hi! I am subhan hare....' }, { Name: "me", Message: 'Walikum.....' }])
+
+  const db = getDatabase();
+
+  let handleSand = () => {
+    console.log(state)
+    const chatListRef = ref(db, 'chat');
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      ...state
+      // ...
+    });
+    setChat([...chat,
+      state
+    ])
+
+  }
+
+  let handleChange = (e) => {
+    setState(
+      {
+        ...state,
+        // Name: 'me',
+        Name: 'me',
+        [e.target.name]: e.target.value
+      }
+    )
+
+
+  }
   return (
     <>
-     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World</h1>
+      <div className='text-black w-screen h-screen bg-slate-400 relative'>
+        <label htmlFor="l">Name:</label>
+        <input type="text" onChange={handleChange} name="Name" id="l" />
+        {
+          chat.map(value => <div className={`border border-black flex ${value.Name == 'me' ? 'flex-row-reverse text-red-800' : " "}`}>
+            <p className='border border-red-700 rounded p-3 max-w-xs  bg-blue-600 m-2'><strong>Name:{value.Name}</strong>
+              {value.Message}
+            </p>
+
+          </div>)
+        }
+        <div className='bottom-0 absolute'>
+          <label htmlFor="f">Message: </label>
+          <input onChange={handleChange} type="text" name="Message" id="f" />
+          <button onClick={handleSand}>sand</button>
+        </div>
+      </div>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDhND7gUwpbKwmN4PdCp4EID76sx6WgtKI",
+  authDomain: "myallchat-2f16c.firebaseapp.com",
+  databaseURL: "https://myallchat-2f16c-default-rtdb.firebaseio.com",
+  projectId: "myallchat-2f16c",
+  storageBucket: "myallchat-2f16c.appspot.com",
+  messagingSenderId: "893817889689",
+  appId: "1:893817889689:web:484a156421812fe3af6908"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

1062e090b27695b60ecd65e05779294b2f3d0d22
Zainqalandar <zainqlandar@gmail.com>
2023-09-05 19:23:54 +0500

    Add Some features

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,71 @@
-import { useEffect, useState } from 'react'
-import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
+
+import { useState, useEffect } from 'react'
 import './App.css'
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
+
 function App() {
   console.log('app')
-  const [name, setName] = useState('Qalandar')
-  const [chat, setChat] = useState([{ Name: "zain", Message: 'Hi! I am zain haree....' }, { Name: "suhan", Message: 'Hi! I am subhan hare....' }])
-  const [mess, setMess] = useState('')
-  const db = getDatabase();
-  const ChatListRef = ref(db, 'CHAT');
-  
+  const [name, setName] = useState()
+  const [chats, setChats] = useState([])
+  const [meg, setMeg] = useState('')
+
+  const database = getDatabase();
+  const chatListRef = ref(database, 'CHATs');
+  const chatRef = push(chatListRef);
+
+  let updatHeight = () => {
+    let el = document.getElementById('scoll')
+    if (el) {
+      el.scrollTop = el.scrollHeight
+    }
+  }
+  document.getElementsByTagName('body')[0].style.background = 'RGB(33, 92, 84)'
   useEffect(() => {
-    onChildAdded(ChatListRef, (data) => {
-      setChat((chat) => [...chat, data.val()]);
+
+    onChildAdded(chatListRef, (data) => {
+      setChats(chats => [...chats, data.val()])
+      setTimeout(() => {
+        updatHeight()
+      }, 100);
     });
 
   }, [])
 
-  let SendChat = () => {
-    const ChatRef = push(ChatListRef);
-    set(ChatRef, {
-      Name: name, Message: mess
-      // ...
-    });
 
+  let sandMeg = () => {
+    set(chatRef, {
+      Name: name, Message: meg
+    });
+    setMeg('')
 
   }
-
   return (
     <>
-      <div className='text-black w-screen h-screen bg-slate-400 relative'>
-        <h1 className='text-4xl'>Name: {name}</h1>
-        <label htmlFor="l">Name:</label>
-        <input type="text" onBlur={e => setName(e.target.value)} name="Name" id="l" />
-        {
-          chat.map((value, i) => <div key={i} className={`border border-black flex ${value.Name == 'me' ? 'flex-row-reverse text-red-800' : " "}`}>
-            <p className='border border-red-700 rounded p-3 max-w-xs  bg-blue-600 m-2'><strong>{value.Name}</strong>
-              {value.Message}
-            </p>
-          </div>)
-        }
-        <div className='bottom-0 absolute'>
-          <label htmlFor="f">Message: </label>
-          <input onInput={e => setMess(e.target.value)} type="text" value={mess} name="Message" id="f" />
-          <button onClick={e => SendChat()}>sand</button>
+      {name ? null : <div>
+        <input className='border' type="text" name="" id="" onBlur={(e) => setName(e.target.value)} />
+      </div>}
+      {name ? <div >
+        <div id='scoll' className='chat-container h-[89vh] overflow-y-scroll'>
+          <h1 className='text-4xl text-center'>Name: {name}</h1>
+          {
+            chats.map((c, i) => <div key={i} className={`container  flex ${c.Name == name ? 'flex-row-reverse ' : ''}`} >
+              <p className={`chatbox border-2  border-gray-400 ${c.Name == name ? 'bg-green-400 rounded-l-full rounded-tr-full ' : " ml-7 bg-white rounded-r-full rounded-tl-full"} p-4 my-2  bg-bisque`}>
+                <i className='text-red-400 text-sm'>{c.Name !== name && '+92174433473'}{c.Name !== name && <i className=' text-slate-400'>~Asad</i>}</i><br />
+                <strong>{c.Name} </strong>
+                {c.Message}
+              </p>
+            </div>)}
+          <div className='border border-red-900  bg-slate-300 p-3 fixed w-full flex bottom-0'>
+            <input className='rounded-lg p-2 flex-grow' type="text" name="" placeholder='Sand Message....' value={meg} onInput={e => setMeg(e.target.value)} id="" />
+            {/* <button  className='' >Sand</button> */}
+            <button className="material-symbols-outlined m-2" onClick={() => sandMeg()}>
+              send
+            </button>
+          </div>
         </div>
-      </div>
-
+      </div> : null}
     </>
+    
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,26 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhND7gUwpbKwmN4PdCp4EID76sx6WgtKI",
-  authDomain: "myallchat-2f16c.firebaseapp.com",
-  databaseURL: "https://myallchat-2f16c-default-rtdb.firebaseio.com",
-  projectId: "myallchat-2f16c",
-  storageBucket: "myallchat-2f16c.appspot.com",
-  messagingSenderId: "893817889689",
-  appId: "1:893817889689:web:484a156421812fe3af6908"
+  apiKey: "AIzaSyB39P0uqDLQhimzjIoW3CA5ZOOImDAwmq4",
+  authDomain: "react-chatapp-de020.firebaseapp.com",
+  databaseURL: "https://react-chatapp-de020-default-rtdb.firebaseio.com",
+  projectId: "react-chatapp-de020",
+  storageBucket: "react-chatapp-de020.appspot.com",
+  messagingSenderId: "688246609786",
+  appId: "1:688246609786:web:e49a46140ff2ce764da8c3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+ 
 )

e8fdc861c19cb8123f3d08908fbd53d6edab124e
Zainqalandar <zainqlandar@gmail.com>
2023-09-08 12:51:50 +0500

    no

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import useCurrency from './hooks/useCurrency'
 
 export default function App() {
   const [amout, setAmout] = useState(1)
-  const [from, setFrom] = useState('inr')
+  const [from, setFrom] = useState('usd')
   const [to, setTo] = useState("pkr")
   const [convertedAmount, setconvertedAmount] = useState()
   const usecurrency = useCurrency(from)
@@ -12,25 +12,43 @@ export default function App() {
   console.log(usecurrency)
   console.log(option)
   let conver=()=>{
-    setconvertedAmount(amout * useCurrency[to])
+    setconvertedAmount(amout * usecurrency[to])
+  }
+  // console.log(convertedAmount)
+  console.log('From:'+ from +' To '+ to)
+  let swap=()=>{
+    setFrom(to)
+    setTo(from)
+    setAmout(convertedAmount)
+    setconvertedAmount(amout)
   }
 
   return (
     <>
     <div className='border-2 border-black w-full h-screen'>
-      <div className='border-2 border-red-400 w-1/3 mx-auto my-9'>
+      <div className='border-2 border-red-400 w-1/3 mx-auto my-9 relative'>
         <form onSubmit={(e)=>e.preventDefault()}>
           <div className=' my-6'>
             <InputBox
+            label='From'
             currencyOptions={option}
+            onCurrencyChange={(currency)=>setFrom(currency)}
+            onAmountChange={(amout)=>setAmout(amout)}
+            amout={amout}
+            defaultCurrency={from}
             ></InputBox>
           </div>
+            <button className=' bg-red-700 p-3 rounded-full absolute  left-1/2 top-20'  onClick={()=>swap()}>Swap</button>
           <div className=' my-6'>
             <InputBox
             currencyOptions={option}
+            label='To'
+            defaultCurrency={to}
+            amout={convertedAmount}
+            onCurrencyChange={(currency)=>setTo(currency)}
             ></InputBox>
           </div>
-          <button className=' bg-blue-700 p-5 rounded-lg' onClick={conver}>Convert USD to PKR {convertedAmount}</button>
+          <button className=' bg-blue-700 p-5 rounded-lg  ml-[30%]' onClick={conver}>Convert USD to PKR</button>
         </form>
       </div>
     </div>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 
 export default function InputBox({
-    currencyOptions
+    label,
+    currencyOptions,
+    onAmountChange,
+    defaultCurrency,
+    onCurrencyChange,
+    amout
 }) {
+    console.log(amout)
     return (
         <>
             <div className='flex justify-center border-2 border-green-800'>
                 <div className=' border border-red-700 rounded-l-lg bg-yellow-500 p-2'>
-                    <label htmlFor="">lable</label><br />
-                    <input type="number" name="" id="" />
+                    <label htmlFor="">{label}</label><br />
+                    <input 
+                    type="number"
+                    value={amout}
+                    onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
+                     />
                 </div>
                 <div className=' border border-red-700 rounded-r-lg bg-yellow-500 p-2'>
                     <p>Currency Type</p>
-                    <select name="" id="">
-                        {currencyOptions.map(currency=><option key={currency} value="usd">
+                    <select name=""
+                    value={defaultCurrency}
+                    onChange={(e)=>onCurrencyChange(e.target.value)}
+                     id="">
+                        {currencyOptions.map(currency=><option key={currency} value={currency}>
                             {currency}
                         </option>)}
                     </select>

8d9e9f07c9651ebd4771dea942281495b9fce2d8
Zainqalandar <zainqlandar@gmail.com>
2023-09-13 12:27:45 +0500

    main

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World Machine chage</h1>
+     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World Machine chage main</h1>
     </>
   )
 }

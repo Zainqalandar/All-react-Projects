9360ab16f40aa4487fab0c9ee950e7afc740f929
Zainqalandar <zainqlandar@gmail.com>
2023-09-13 10:28:30 +0500

    Two change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 function App() {
   return (
     <>
-     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World one change</h1>
+     <h1 className='border border-black text-center text-lg bg-yellow-600'>Hello World one change two change</h1>
     </>
   )
 }
